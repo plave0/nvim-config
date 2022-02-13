7e0127772cb69225538408b5fc13db6c518f8dfb
Pavle Ciric <pavle.ciric@hotmail.com>
2022-02-13 11:17:03 +0100

    [add] basic options

---
+++ lua/user/options.lua
@@ -0,0 +1,19 @@
+local o = vim.opt
+
+o.backup = false		
+o.swapfile = false
+
+o.shiftwidth = 4
+o.tabstop = 2
+o.number = true
+o.wrap = false
+o.smartindent = true
+o.ignorecase = true
+o.showtabline = 2
+
+o.splitbelow = true
+o.splitright = true
+
+o.fileencoding = "utf-8" 
+o.clipboard = "unnamedplus"
+o.mouse = "a"
