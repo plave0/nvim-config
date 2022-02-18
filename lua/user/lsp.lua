734ab1dcc17f38ce24143553531b99d0fa3ae514
Pavle Ciric <pavle.ciric@hotmail.com>
2022-02-18 17:44:27 +0100

    [add] lsp highlight

--- lua/user/lsp.lua
+++ lua/user/lsp.lua
@@ -22,6 +22,21 @@ local on_attach = function(client, bufnr)
   --vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>f', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
 	vim.api.nvim_buf_set_keymap(bufnr, "n", "gl",
 		'<cmd>lua vim.diagnostic.open_float()<CR>', opts)
+
+  -- Lsp highlight
+  if client.resolved_capabilities.document_highlight then
+    vim.api.nvim_exec(
+      [[
+      augroup lsp_document_highlight
+        autocmd! * <buffer>
+        autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
+        autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
+      augroup END
+    ]],
+      false
+    )
+  end
+
 end
 
 -- lua lsp configuration
@@ -50,7 +65,6 @@ local lua_settings = {
     },
   }
 }
-
 
 -- nvim-lsp-installer of attaching lsp client
 lsp_installer.on_server_ready(function(server)
