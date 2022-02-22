f975f5010dba7f3219b5ef3c27c3bd30e7562f36
Pavle Ciric <pavle.ciric@hotmail.com>
2022-02-22 19:43:50 +0100

    [edit] remove lsp highlight

--- lua/user/lsp.lua
+++ lua/user/lsp.lua
@@ -23,20 +23,6 @@ local on_attach = function(client, bufnr)
 	vim.api.nvim_buf_set_keymap(bufnr, "n", "gl",
 		'<cmd>lua vim.diagnostic.open_float()<CR>', opts)
 
-  -- Lsp highlight
-  if client.resolved_capabilities.document_highlight then
-    vim.api.nvim_exec(
-      [[
-      augroup lsp_document_highlight
-        autocmd! * <buffer>
-        autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
-        autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
-      augroup END
-    ]],
-      false
-    )
-  end
-
 end
 
 -- lua lsp configuration
