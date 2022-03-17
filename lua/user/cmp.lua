85cb07908dc42353e0e5e98a17818458fad1ad62
Pavle Ciric <pavle.ciric@hotmail.com>
2022-03-17 11:24:27 +0100

    [fix] cmp icons bug

--- lua/user/cmp.lua
+++ lua/user/cmp.lua
@@ -23,31 +23,31 @@ local check_backspace = function()
   return col == 0 or vim.fn.getline("."):sub(col, col):match "%s"
 end
 
-local kind_icons = { Text = "",
+local kind_icons = { Text = " ",
   Method = "m",
-  Function = "",
-  Constructor = "",
-  Field = "",
-  Variable = "",
-  Class = "",
-  Interface = "",
-  Module = "",
-  Property = "",
-  Unit = "",
-  Value = "",
-  Enum = "",
-  Keyword = "",
-  Snippet = "",
-  Color = "",
-  File = "",
-  Reference = "",
-  Folder = "",
-  EnumMember = "",
-  Constant = "",
-  Struct = "",
-  Event = "",
-  Operator = "",
-  TypeParameter = "",
+  Function = " ",
+  Constructor = " ",
+  Field = " ",
+  Variable = "",
+  Class = " ",
+  Interface = " ",
+  Module = " ",
+  Property = " ",
+  Unit = " ",
+  Value = " ",
+  Enum = " ",
+  Keyword = " ",
+  Snippet = " ",
+  Color = " ",
+  File = " ",
+  Reference = " ",
+  Folder = " ",
+  EnumMember = " ",
+  Constant = " ",
+  Struct = " ",
+  Event = " ",
+  Operator = " ",
+  TypeParameter = " ",
 }
 -- find more here: https://www.nerdfonts.com/cheat-sheet
 
@@ -95,7 +95,7 @@ cmp.setup {
       else
         fallback()
       end
-    end, {
+    end,{
       "i",
       "s",
     }),
