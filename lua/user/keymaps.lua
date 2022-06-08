6d9cd329cb0dd46a700651ea56a5fdb7ce327fec
Pavle Ciric <pavle.ciric@hotmail.com>
2022-06-08 22:50:22 +0200

    [edit] Keymaps cleanup

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -1,16 +1,9 @@
 local opts = { noremap = true, silent = true }
 
--- Shortcut
 local keymap = vim.api.nvim_set_keymap
 
--- Leader
 keymap("", "<Space>", "<Nop>", opts)
 vim.g.mapleader = " "
-
--- Normal --
-
-keymap("n", "<leader>l", "gt", opts)
-keymap("n", "<leader>h", "gT", opts)
 
 -- Window navigation
 keymap("n", "<C-h>", "<C-w>h", opts)
@@ -28,24 +21,10 @@ keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
 keymap("n", "<S-l>", ":bnext<CR>", opts)
 keymap("n", "<S-h>", ":bprevious<CR>", opts)
 
--- Move text up and down
-keymap("n", "<A-j>", "<Esc>:m .+1<CR>==gi", opts)
-keymap("n", "<A-k>", "<Esc>:m .-2<CR>==gi", opts)
-
--- Visual --
-
 -- Stay in indent mode
-keymap("v", "<", "<gv", opts)
 keymap("v", ">", ">gv", opts)
+keymap("v", "<", "<gv", opts)
 
--- Move text up and down
-keymap("v", "<A-j>", ":m .+1<CR>==", opts)
-keymap("v", "<A-k>", ":m .-2<CR>==", opts)
-keymap("v", "p", '"_dP', opts)
-
--- Visual Block --
 -- Move text up and down
 keymap("x", "J", ":move '>+1<CR>gv-gv", opts)
 keymap("x", "K", ":move '<-2<CR>gv-gv", opts)
-keymap("x", "<A-j>", ":move '>+1<CR>gv-gv", opts)
-keymap("x", "<A-k>", ":move '<-2<CR>gv-gv", opts)
