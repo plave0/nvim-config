6c0e47dadc1e6a9ffe00de56b4ddee7839aa354c
Pavle Ciric <pavle.ciric@hotmail.com>
2022-06-08 20:51:34 +0200

    [add] Install cmp_lsp_signatures_help

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -47,6 +47,7 @@ return packer.startup(function(use)
 	use "hrsh7th/cmp-nvim-lua"
 	use 'saadparwaiz1/cmp_luasnip'
   use "hrsh7th/cmp-nvim-lsp"
+  use "hrsh7th/cmp-nvim-lsp-signature-help"
 
   -- navigation
 	use "nvim-telescope/telescope.nvim"
