5f7d8bdf9d321cb9b0de48484e0f8efc995a1710
Pavle Ciric <pavle.ciric@hotmail.com>
2022-06-08 21:19:52 +0200

    [add] Initial autopair configuration

--- lua/user/autopair.lua
+++ lua/user/autopair.lua
@@ -1,1 +1,6 @@
-require("nvim-autopairs").setup {}
+local status_ok, nvim_autopair = pcall(require, 'nvim-autopairs')
+if not status_ok then
+  vim.notify("Vim Autopair not loaded!")
+end
+
+nvim_autopair.setup()
