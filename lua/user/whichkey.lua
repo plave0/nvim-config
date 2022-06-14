38443cf9df27e904390ade2c6b9a8563d5c76fe3
Pavle Ciric <pavle.ciric@hotmail.com>
2022-06-14 22:42:07 +0200

    [edit] Whichkey reorganize mappings

--- lua/user/whichkey.lua
+++ lua/user/whichkey.lua
@@ -10,22 +10,37 @@ which_key.setup({
   }
 })
 
-which_key.register({
-  s = {
-    name = "Search",
-    f = {"<cmd>Telescope find_files<cr>", "Srach files"},
-    l = {"<cmd>Telescope live_grep<cr>", "Live grep"},
-    b = {"<cmd>Telescope buffers<cr>", "Srach buffers"},
-    h = {"<cmd>Telescope help_tags<cr>", "Srach help"},
-    d = {"<cmd>Telescope diagnostic<cr>", "Srach diagnostics"}
-  }
-})
-
-which_key.register({
-    e = {"<cmd>NvimTreeToggle<cr>", "Toggle file explorer"},
-    h = {
-      n = {"<cmd>Gitsigns next_hunk<cr>", "Next hunk"},
-      p = {"<cmd>Gitsigns prev_hunk<cr>", "Prev hunk"},
-      h = {"<cnd>Gitsigns preview_hunk<cr>", "Preview hunk"}
+local set_telescope_keys = function()
+  which_key.register({
+    s = {
+      name = "Search",
+      f = {"<cmd>Telescope find_files<cr>", "Srach files"},
+      l = {"<cmd>Telescope live_grep<cr>", "Live grep"},
+      b = {"<cmd>Telescope buffers<cr>", "Srach buffers"},
+      h = {"<cmd>Telescope help_tags<cr>", "Srach help"},
+      d = {"<cmd>Telescope diagnostic<cr>", "Srach diagnostics"}
     }
-}, {mode = "n", prefix = "<leader>"} )
+  })
+end
+
+local set_nvim_tree_keys = function()
+  which_key.register({
+      e = {"<cmd>NvimTreeToggle<cr>", "Toggle file explorer"},
+    }, {mode = "n", prefix = "<leader>"})
+end
+
+local set_git_signs_keys = function()
+  which_key.register({
+      h = {
+        n = {"<cmd>Gitsigns next_hunk<cr>", "Next hunk"},
+        p = {"<cmd>Gitsigns prev_hunk<cr>", "Prev hunk"},
+        h = {"<cnd>Gitsigns preview_hunk<cr>", "Preview hunk"}
+      }
+  }, {mode = "n", prefix = "<leader>"} )
+end
+
+set_telescope_keys()
+set_nvim_tree_keys()
+set_git_signs_keys()
+
+
